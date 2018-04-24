f6b669f428ec1ba6695b35ed265bc8ed30792aab
Marcel Beaulieu <Marcel.Beaulieu@umontreal.ca>
2018-04-24 16:23:15 -0400

    Création de l'écran principal =============================
    Création des boutons et d'un squelette, qui sera utilisé pour l'animation.
    
    Logiciel version: 24 Avril 2018
    Unity version: 2017.4.1f1 (64-bit)
    Version "Windows Installer": non disponible

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Main : MonoBehaviour {
+
+	void Start ()
+	{ }
+	
+	void Update ()
+	{ }
+
+	public void quit()
+	{
+		Application.Quit();
+	}
+}
