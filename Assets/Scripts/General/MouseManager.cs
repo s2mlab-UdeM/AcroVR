57287cf2401c37fdf42eacbe898c6efd07072656
Marcel Beaulieu <Marcel.Beaulieu@umontreal.ca>
2019-05-29 14:50:22 -0400

    Finaliser le bon fonctionnement de l'option Paramètres du graphique, corriger un bug relatif au menu Ajouter/effacer un noeud et notes pour les utilisateurs Mac ================================================================================================================================================================
    1) Logiciel version: NN Mai 2019. Unity version: Unity 2017.4.26f1 (64-bit) LTS. Version "Build" disponible sur le serveur (Mac & Windows): E:\Build\AcroVR\2019.05.NN.
    
    2) Terminer le plein fonctionnement de l'option Axe horizontal des Paramètres du graphique.
    	Ajouter une nouvelle option, Mise à jour simulation.
    	Mise à jour simulation indique que les données de simulation sont recalculé avec la nouvelle durée de cette simulation.
    	Si la borne supérieur est augmenté, alors il faut obligatoirement faire une mise à jour de la simulation.
    	Si la borne supérieur est diminué, alors vous pouvez choisir de mettre à jour la simulation ou pas, sans conséquence.
    
    3) Ajouter 4 boutons pour l'axe vertical des paramètres du graphique.
    	Ces boutons permettent de décrémenter ou incrémenter les bornes inférieur et supérieur de l'axe vertical par un nombre fixe de degrés (-10 ou +10).
    
    4) Ajouter un bouton Annuler aux Paramètres du graphique.
    	Si ce bouton est appuyé, alors la fenêtre des Paramètres du graphique est fermé sans faire aucune modification aux échelles des 2 axes.
    
    5) Ajouter un bouton Valeurs de défault aux Paramètres du graphique.
    	Si ce bouton est appuyé, alors toutes les bornes (inférieur et supérieur vertical et supérieur horizontal) sont remise aux valeurs des bornes lorsque
    	le graphique de l'articulation a été chargé (valeurs calculées automatiquement par le logiciel selon les données affichées).
    
    6) Corriger le fonctionnement de la barre de défilement vertical des Paramètres du graphique, quand on utilise les flèches gauche et droite.
    
    7) Corriger un bug. Effacé le menu Ajouter/effacer un noeud quand on clique sur aucun des boutons affichés.
    	Quand le bouton droit de la souris est appuyé au-dessus du graphique, alors le menu Ajouter/effacer un noeud est affiché.
    	Si un utilisateur clique (bouton gauche de la souris) sur un des boutons affichés, alors l'action est exécuté correctement.
    	Mais si l'utilisateur clique sur aucun des boutons affichés, alors rien n'était fait.
    	Maintenant, le menu est effacé, ce qui semble plus logique d'après moi.
    
    8) Note pour les utilisateurs Mac.
    	J'ai remarqué que le rendu de l'animation n'était pas correct sur le MacBook que j'ai utilisé, si la résolution et la qualité de l'affichage étaient
    		trop grande. Mais en diminuant ces paramètres, alors l'animation fonctionne très bien.
    	J'ai aussi remarqué que le temps d'exécution de l'animation n'était pas correcte. J'avais pas fait attention à cela avant, donc il est possible que
    		ce bug était là même dans les versions précédentes. À corriger dans une version future.

---
+++ Assets/Scripts/General/MouseManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// =================================================================================================================================================================
+/// <summary> Contrôle de certaines interactions de la souris avec les objets (GameObjects). </summary>
+
+public class MouseManager : MonoBehaviour
+{
+	public static MouseManager Instance;
+	public Canvas canvas;
+
+	// =================================================================================================================================================================
+	/// <summary> Initialisation du script. </summary>
+
+	void Start()
+	{
+		Instance = this;
+	}
+
+	// =================================================================================================================================================================
+	/// <summary> Vérifier si la souris est au-dessus (retourne TRUE) ou à côté (retourne FALSE) de l'objet spécifié. </summary>
+
+	public bool IsOnGameObject(GameObject gameObject)
+	{
+		Vector2 inputMousePos = Input.mousePosition;
+		Vector3[] menuPos = new Vector3[4];
+		gameObject.GetComponent<RectTransform>().GetWorldCorners(menuPos);
+		Vector3[] gameObjectPos = new Vector3[2];
+		gameObjectPos[0] = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, menuPos[0]);
+		gameObjectPos[1] = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, menuPos[2]);
+		return (inputMousePos.x >= gameObjectPos[0].x && inputMousePos.x <= gameObjectPos[1].x && inputMousePos.y >= gameObjectPos[0].y && inputMousePos.y <= gameObjectPos[1].y);
+	}
+}
